baa89825145470a805aadbdcd344f6da7ba42b68
adashbob <bobodiallobe@gmail.com>
2016-03-02 16:17:06 +0000

    First Commit

---
+++ core/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+// Entry file to the application
+require_once __DIR__.'/config.inc.php';
+require_once __DIR__.'/../vendor/autoload.php';
+
+
+use RedBeanPHP\Facade as R;
+
+if(empty(R::$currentDB)){
+    $dsn = sprintf('%s:host=%s;dbaname=%s', DB_TYPE, DB_HOST, DB_NAME);
+    R::setup($dsn, DB_USER, DB_PASSWORD);
+}
+R::close();
---
+++ core/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Database configuration
+ */
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'collectify');
+define('DB_NAME', 'collectify');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'bdiallo');
+
+/**
+ * Redbean configuration
+ */
+define('REDBEAN_MODEL_PREFIX', '\\Collectify\\Model');
+    
