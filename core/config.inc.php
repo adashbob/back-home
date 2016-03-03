d0eff9b3e330201d2240f51931d190bebf3f5c41
adashbob <bobodiallobe@gmail.com>
2016-03-03 11:40:36 +0000

    DataFixtures

--- core/config.inc.php
+++ core/config.inc.php
@@ -7,7 +7,7 @@ define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'collectify');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'bdiallo');
+define('DB_PASSWORD', '');
 
 /**
  * Redbean configuration
